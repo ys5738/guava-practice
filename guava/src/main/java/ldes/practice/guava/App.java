3acda4601a59e6fb7608e21a4884fd28fd6f24ff
yangsong <573869800@qq.com>
2018-04-25 15:57:35 +0800

    add MutliMap

--- guava/src/main/java/ldes/practice/guava/App.java
+++ guava/src/main/java/ldes/practice/guava/App.java
@@ -1,5 +1,10 @@
 package ldes.practice.guava;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.hash.Hashing;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Map<String, Object> params = new HashMap<>();
+    	params.put("a", 123);
+    	params.put("b", "Asc");
+    	params.put("c", "asdaw123");
+    	System.out.println(params.toString());
+        System.out.println();
     }
 }
